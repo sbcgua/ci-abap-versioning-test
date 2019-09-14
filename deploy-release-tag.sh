52b4cdaa6ec8ae1fabdd59654f621d7e47b0cc08
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2019-09-14 16:02:04 +0300

    minor tweaks and rename

--- deploy-release-tag.sh
+++ deploy-release-tag.sh
@@ -2,6 +2,7 @@
 echo "Detecting version change ..."
 
 VERSION_FILE=src/zif_abapgit_version.intf.abap
+VERSION_CONSTANT=gc_abap_version
 
 git diff-tree --no-commit-id --name-only -r HEAD | grep $VERSION_FILE > /dev/null 2>&1
 if [ $? -ne 0 ]; then
@@ -11,18 +12,18 @@ fi
 
 VERSION_DIFF=$(git diff HEAD^:$VERSION_FILE HEAD:$VERSION_FILE)
 
-echo "$VERSION_DIFF" | grep gc_abap_version > /dev/null 2>&1
+echo "$VERSION_DIFF" | grep $VERSION_CONSTANT > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "no version change detected, skipping tag creation"
     exit 0
 fi
 
-VERSION_BEFORE=$(echo "$VERSION_DIFF" | grep '^-.\+\bgc_abap_version\b' | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+')
-VERSION_AFTER=$(echo "$VERSION_DIFF" | grep '^+.\+\bgc_abap_version\b' | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+')
+VERSION_BEFORE=$(echo "$VERSION_DIFF" | grep '^-.\+\b$VERSION_CONSTANT\b' | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+')
+VERSION_AFTER=$(echo "$VERSION_DIFF" | grep '^+.\+\b$VERSION_CONSTANT\b' | grep -E -o '[0-9]+\.[0-9]+\.[0-9]+')
 
 if [ -z $VERSION_BEFORE ] || [ -z $VERSION_AFTER ]; then
     echo "unexpected version parsing error"
-    echo "$VERSION_DIFF" | grep gc_abap_version
+    echo "$VERSION_DIFF" | grep $VERSION_CONSTANT
     exit 1
 fi
 
@@ -32,7 +33,7 @@ if [ $VERSION_BEFORE = $VERSION_AFTER ]; then
 fi
 
 TAG="v$VERSION_AFTER"
-echo "version change detected [$VERSION_BEFORE > $VERSION_AFTER], creating a new tag $TAG ..."
+echo "version change detected [$VERSION_BEFORE > $VERSION_AFTER], creating a new tag ..."
 
 # DEPLOY
 
