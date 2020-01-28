6727eabab1779d446660cc949fbde0fe1a76644c
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2020-01-28 23:56:43 +0200

    es6

--- docs/demo.js
+++ docs/demo.js
@@ -1,5 +1,5 @@
 document.getElementById("text2").innerHTML = "And this one too";
-$().ready(function() {
+$().ready(() => {
     $("#text3").html("This is text3");
     $("#textInc").html("This is text in includes");
- });
+});
